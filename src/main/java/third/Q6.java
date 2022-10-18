66c4c11689170923f8ad487f1a19b568b0f2a79f
space8033 <space8237@naver.com>
2022-10-18 11:00:17 +0900

    프로그래머스 num이 기본설정에 int로 되어있어서 그대로 풀었더니 틀림. int와 long차이인데 값이 다르게나옴...

--- src/main/java/third/Q6.java
+++ src/main/java/third/Q6.java
@@ -1,23 +1,23 @@
-//package third;
-//
-//public class Q6 {
-//    public int solution(long num) {
-//        int answer=0;
-//
-//        while (num!=1) {
-//            if(num%2 == 0) {
-//                num /= 2;
-//            } else {
-//                num = 3*num + 1;
-//            }
-//            answer ++;
-//
-//            if(answer==500) {
-//                answer = -1;
-//                break;
-//            }
-//        }
-//
-//        return answer;
-//    }
-//}
+package third;
+
+public class Q6 {
+    public int solution(long num) {
+        int answer=0;
+
+        while (num!=1) {
+            if(num%2 == 0) {
+                num /= 2;
+            } else {
+                num = 3*num + 1;
+            }
+            answer ++;
+
+            if(answer==500) {
+                answer = -1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
