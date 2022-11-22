db041b37f64334a21252578cdd73b8473ac3e458
space8033 <space8237@naver.com>
2022-11-22 17:27:36 +0900

    1122 done

---
+++ array/array2D/TriangularSnail.java
@@ -0,0 +1,36 @@
+public class TriangularSnail {
+    public int[] solution(int n) {
+        int[] answer = new int[n * (n+1) / 2];
+        int[][] board = new int[n][n];
+
+        int x = -1;
+        int y = 0;
+        int num = 1;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(i % 3 == 0) {
+                    x++;
+                }else if(i % 3 == 1) {
+                    y++;
+                }else if(i % 3 == 2) {
+                    x--;
+                    y--;
+                }
+                board[x][y] = num++;
+            }
+        }
+
+        int k = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 0) {
+                    break;
+                }
+                answer[k++] = board[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ string/MenuRenewal.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class MenuRenewal {
+    List<String> answerList = new ArrayList<>();
+    Map<String, Integer> hashMap = new HashMap<>();
+
+    public String[] solution(String[] orders, int[] course) {
+        for(int i = 0; i < orders.length; i++) {
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            orders[i] = String.valueOf(arr);
+        }
+
+        for(int i = 0; i < course.length; i++) {
+            for(int j = 0; j < orders.length; j++) {
+                combination("", orders[j], course[i]);
+            }
+
+            if(!hashMap.isEmpty()) {
+                List<Integer> countList = new ArrayList<>(hashMap.values());
+                int max = Collections.max(countList);
+
+                if(max > 1) {
+                    for(String key : hashMap.keySet()) {
+                        if(hashMap.get(key) == max) {
+                            answerList.add(key);
+                        }
+                    }
+                }
+                hashMap.clear();
+            }
+        }
+
+        Collections.sort(answerList);
+        String[] answer = new String[answerList.size()];
+
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+
+    public void combination(String order, String others, int count) {
+        if(count == 0) {
+            hashMap.put(order, hashMap.getOrDefault(order,0) + 1);
+            return;
+        }
+
+        for(int i = 0; i < others.length(); i++) {
+            combination(order + others.charAt(i), others.substring(i + 1), count - 1);
+        }
+    }
+}
