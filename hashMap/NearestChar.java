d4ab036b4fca1e8bc82cf548a21f5bf691bf34a4
space8033 <space8237@naver.com>
2022-12-11 15:45:25 +0900

    1211 done

---
+++ hashMap/NearestChar.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class NearestChar {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(!map.containsKey(s.charAt(i))) {
+                answer[i] = -1;
+                map.put(s.charAt(i), i);
+            }else {
+                answer[i] = i - map.get(s.charAt(i));
+                map.put(s.charAt(i), i);
+            }
+        }
+
+        return answer;
+    }
+}
